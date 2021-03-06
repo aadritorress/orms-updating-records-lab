7dcc5be3d9527ead0fa251bd5fc3c38eb87c6982
Adriana <adriana.amaral@msn.com>
2021-03-06 15:19:38 -0600

    commit tree ae1e16736445c7057ec8719b32a62504249b7a66

--- lib/student.rb
+++ lib/student.rb
@@ -2,8 +2,63 @@ require_relative "../config/environment.rb"
 
 class Student
 
+  attr :id, :name, :grade
+
+  def initialize(id =nil, name, grade)
+    @id = id 
+    @name = name 
+    @grade = grade
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
-
-
 end
+
+
+def self.create_table
+  sql = <<-SQL
+  CREATE TABLE students (
+  id INTEGER PRIMARY KEY,
+  name TEXT,
+  grade INTEGER
+); 
+SQL
+
+DB[:conn].execute(sql)
+end
+
+
+def self.drop_table
+  sql = <<-SQL
+  DROP TABLE students 
+SQL
+
+DB[:conn].execute(sql)
+end
+
+
+def save 
+  sql = <<-SQL
+  INSERT INTO students (name, grade) 
+  VALUES (?, ?)
+SQL
+
+DB[:conn].execute(sql, self.name, self.grade)
+ end
+
+
+def self.create
+
+
+end 
+
+def self.find_by_name(name)
+  sql = <<-SQL
+  SELECT *
+  FROM students
+  WHERE name = ?
+SQL
+
+DB[:conn].execute(sql, name).map do |row|
+  self.new_from_db(row)
+end
+end
+end 
